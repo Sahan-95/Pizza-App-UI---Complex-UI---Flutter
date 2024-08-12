24536d16a5459eecc89f278621241219f97e3822
Sahan Fernando <sahan@Sahans-MacBook-Air.local>
2024-08-12 19:42:31 +0530

    Completed Pizza complex UI

---
+++ lib/utils/data.dart
@@ -0,0 +1,49 @@
+import 'package:pizza_app/model/pizza_model.dart';
+
+final List<Pizza> pizzaList = [
+  Pizza(
+      name: "Italian Tuna",
+      image: "assets/images/italian_tuna.png",
+      price: "33.40",
+      description:
+          "Italian tuna pizza features a crispy crust topped with rich tomato sauce, melted mozzarella, flavorful tuna, and a blend of Mediterranean herbs."),
+  Pizza(
+      name: "Four Cheese",
+      image: "assets/images/four_chees.png",
+      price: "18.50",
+      description:
+          "The Mozzarella pizza is a quintessential Italian treat, boasting a crisp crust layered with savory tomato sauce, melted mozzarella cheese, and a dash of fragrant herbs. This classic favorite blends the freshness of simple ingredients with the comforting essence of traditional pizza, making it a cherished option for any gathering."),
+  Pizza(
+      name: "Mozzarella",
+      image: "assets/images/mozzarella.png",
+      price: "15.99",
+      description:
+          "Pizza is a gastronomic delight that unites the rich and unique flavors of four exquisite cheeses on a golden, oven-baked crust. Savor the luxurious blend of creamy mozzarella, sharp cheddar, tangy Parmesan, and smooth provolone, all harmoniously balanced by a savory tomato sauce."),
+  Pizza(
+      name: "Salami",
+      image: "assets/images/salami.png",
+      price: "25.30",
+      description:
+          "The Salami Supreme is a delectable pizza treat that offers a perfect blend of savory flavors and zesty spices. Our specially crafted dough is baked to golden perfection, providing the base for a tempting mix of rich tomato sauce, gooey melted cheese, and thinly sliced, flavorful salami."),
+  Pizza(
+      name: "Quattro Stagioni",
+      image: "assets/images/quattro_stagioni.png",
+      price: "39.80",
+      description:
+          "Quattro Stagioni pizza is a delightful Italian classic that divides its toppings into four sections, each representing a different season. It typically features artichokes, ham, mushrooms, and olives, all arranged on a bed of tomato sauce and mozzarella cheese."),
+  Pizza(
+      name: "Capricciosa",
+      image: "assets/images/capricciosa.png",
+      price: "29.90",
+      description:
+          "CapriClassic Pizza is a culinary masterpiece that captures the essence of Italian tradition and flavor. Built on a foundation of hand-stretched dough, this pizza is topped with a medley of premium ingredients, including savory ham, earthy mushrooms, tangy artichoke hearts, and briny olives. All of these delicious toppings rest on a bed of rich tomato sauce and gooey mozzarella cheese."),
+  Pizza(
+      name: "Prosciutto",
+      image: "assets/images/prosciutto.png",
+      price: "25.00",
+      description:
+          "Prosciutto Bliss is an exquisite pizza creator that Celebrates the delicate yet flavorfull inssence of thinly sliced presciutto Crafted on a foundation of artisanal dough, this pizza masterpiece is adorned with luscious layer of tangy tomato sauce and creamy mozzarella cheese perfectly complementing the sarty richness of the prosciutto"),
+];
+
+
+final pizzaSizes = ['S', 'M', 'L'];
